320c67e970cafe3e8a6b69901f0bfee4f8d05c0a
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-29 20:50:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,3 +35,7 @@ parseInt(n, 10)
 function preserveDecimal(n){
 return parseFloat(n)
 }
+
+function makeInt(a){ //Parse a as an integer and return that integer.
+  number = parseInt(a, 10);
+  return number;}
