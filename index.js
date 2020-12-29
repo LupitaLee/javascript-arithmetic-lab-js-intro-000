4c2d384f20a1efe5c4510057ff45ede938fd6a4d
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-29 20:54:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
-function add() {1+8}
-function subtract() {60-40}
-function multiply() {2* 3.4}
-function divide() {5.0 / 2.5}
-function add(a,b) {return a+b}
-function subtract(a,b){return a-b}
-function multiply(a,b){return a*b}
-function divide(a,b) {return a/b}
-function decreament() {var n=0;
-n--;
-return n;}
-
-var i = 0;
-
-function increment(n){
-
-  n++;
-  return n;
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
+  return number;
 }
-
-var n = 0;
-
-function decrement(n){
-
-  n--;
-  return n;
+function subtract(a, b){ //subtract a and b and return the result.
+  number = a - b;
+  return number;
 }
-
-function makeInt(n){return parseInt;
+function multiply(a, b){ //multiply a and b and return the result.
+  number = a * b;
+  return number;
 }
-
-function makeInt(n){
-parseInt(n, 10)
+function divide(a, b){ //divide a and b and return the result.
+  number = a / b;
+  return number;
 }
-function preserveDecimal(n){
-return parseFloat(n)
+function inc(a){ //Increment a by 1 and return the result.
+  number = a + 1;
+  return number;
 }
-
-function makeInt(n){
-parseInt(563, 10)
+function dec(a){ //Decrement a by 1 and return the result.
+  number = a - 1;
+  return number;
+}
+function makeInt(a){ //Parse a as an integer and return that integer.
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(a, 10);
+  return number;
 }
