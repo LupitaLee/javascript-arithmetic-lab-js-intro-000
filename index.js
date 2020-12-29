27ef33125df9895c2d465bf9b990f7e692dc42a3
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-29 20:43:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,44 @@
+function add() {1+8}
+function subtract() {60-40}
+function multiply() {2* 3.4}
+function divide() {5.0 / 2.5}
+function add(a,b) {return a+b}
+function subtract(a,b){return a-b}
+function multiply(a,b){return a*b}
+function divide(a,b) {return a/b}
+function decreament() {var n=0;
+n--;
+return n;}
+
+var i = 0;
+
+function increment(n){
+
+  n++;
+  return n;
+}
+
+var n = 0;
+
+function decrement(n){
+
+  n--;
+  return n;
+}
+
+function makeInt(n){return parseInt;
+}
+
+function makeInt(n){
+parseInt(n, 10)
+}
+function preserveDecimal(n){
+return parseFloat(n)
+}
+
+function makeInt(n){
+parseInt(563, 10)
+}
+
+makeInt(n){
+parseInt('0', 10);}
